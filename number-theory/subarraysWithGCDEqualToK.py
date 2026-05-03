1f94a220d291830906a9acdf41441d0a253902a5
Jean Claude ISHIMWE <ishclaude88@gmail.com>
2026-05-03 23:50:28 +0200

    Add solution for subarray GCD problem

---
+++ number-theory/subarraysWithGCDEqualToK.py
@@ -0,0 +1,17 @@
+from math import gcd
+from typing import List
+
+
+class Solution:
+    def subarrayGCD(self, nums: List[int], k: int) -> int:
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            g = 0
+            for j in range(i, n):
+                if nums[j] % k != 0:
+                    break
+                g = gcd(g, nums[j])
+                if g == k:
+                    count += 1
+        return count
