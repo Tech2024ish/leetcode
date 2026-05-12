c8c12fee84d61455a77db6bd8e06d7906d27c6dc
Jean Claude ISHIMWE <ishclaude88@gmail.com>
2026-05-12 08:35:32 +0200

    solved majority element leetcode(168)

---
+++ Counting/majority_wlwmwnt.py
@@ -0,0 +1,11 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+        for key, value in freq.items():
+            if value > len(nums)/2:
+                return key
+        return None
