d3728d78b1efb4933694a0666df68a43189cfe4f
Jean Claude ISHIMWE <ishclaude88@gmail.com>
2026-05-13 23:18:30 +0200

    Solved reordered power of 2. Leetcode(869)

---
+++ Counting/reordered_power_of_2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        target = sorted(str(n))
+        for i in range(31):
+            power = 1 << i
+            if sorted(str(power)) == target:
+                return True
+        return False
