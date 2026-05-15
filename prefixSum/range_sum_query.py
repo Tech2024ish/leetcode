bb007eb7df39c689354df0ad31bb56baba2b4d9d
Jean Claude ISHIMWE <ishclaude88@gmail.com>
2026-05-15 22:05:56 +0200

    Solved range sum query-immutable. Leetcode(303)

---
+++ prefixSum/range_sum_query.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.prefix = [0] * (len(nums)+1)
+        for i, num in enumerate(nums):
+            self.prefix[i+1] = self.prefix[i]+num
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right+1] - self.prefix[left]
