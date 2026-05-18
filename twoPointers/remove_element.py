d62b896a5b9feda29134febeb400e111354128bc
Jean Claude ISHIMWE <ishclaude88@gmail.com>
2026-05-18 22:51:36 +0200

    Solved remove element. Leetcode(27)

---
+++ twoPointers/remove_element.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+
+nums = [3, 2, 2, 3]
+val = 3
+s = Solution()
+print(s.removeElement(nums, val))
