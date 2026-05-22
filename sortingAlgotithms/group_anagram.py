6cd025c5e57a68394e46974d6b81b003739e9297
Jean Claude ISHIMWE <ishclaude88@gmail.com>
2026-05-22 18:07:30 +0200

    Solved group anagram. Leetcode(49)

---
+++ sortingAlgotithms/group_anagram.py
@@ -0,0 +1,11 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        fre = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            fre[key].append(s)
+        return list(fre.values())
